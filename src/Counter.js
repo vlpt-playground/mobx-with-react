9e03bf391f30c4c6729992b29f7b02e94c41a2ad
velopert <public.velopert@gmail.com>
2018-08-04 01:04:33 +0900

    counter with separated store

--- src/Counter.js
+++ src/Counter.js
@@ -1,41 +1,23 @@
 import React, { Component } from 'react';
-import { observable, action } from 'mobx';
-import { observer } from 'mobx-react';
+import { observer, inject } from 'mobx-react';
 
-// **** 최하단에 잇던 observer 가 이렇게 위로 올라옵니다.
+@inject(stores => ({
+  number: stores.counter.number,
+  increase: stores.counter.increase,
+  decrease: stores.counter.decrease,
+}))
 @observer
 class Counter extends Component {
-  @observable number = 0;
-
-  @action
-  increase = () => {
-    this.number++;
-  }
-
-  @action
-  decrease = () => {
-    this.number--;
-  }
-
   render() {
+    const { number, increase, decrease } = this.props;
     return (
       <div>
-        <h1>{this.number}</h1>
-        <button onClick={this.increase}>+1</button>
-        <button onClick={this.decrease}>-1</button>
+        <h1>{number}</h1>
+        <button onClick={increase}>+1</button>
+        <button onClick={decrease}>-1</button>
       </div>
     );
   }
 }
 
-
-// **** decorate 는 더 이상 필요 없어집니다.
-// decorate(Counter, {
-//   number: observable,
-//   increase: action,
-//   decrease: action
-// })
-
-// export default observer(Counter);
-// **** observer 는 코드의 상단으로 올라갑니다.
 export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'mobx-react'; // MobX 에서 사용하는 Provider
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import CounterStore from './stores/counter'; // 방금 만든 스토어 불러와줍니다.
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const counter = new CounterStore(); // 스토어 인스턴스를 만들고
+
+ReactDOM.render(
+  <Provider counter={counter}>
+    {/* Provider 에 props 로 넣어줍니다. */}
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
